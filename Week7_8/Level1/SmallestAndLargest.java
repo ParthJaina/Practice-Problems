2e0d441424858a173d36ab749f3be46c79f04d86
ParthJaina <pj2752@srmist.edu.in>
2026-04-02 00:41:50 +0530

    Add Week 7&8 Level 1 Practice Programs (3 programs)

---
+++ Week7_8/Level1/SmallestAndLargest.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class SmallestAndLargest {
+    public static int[] findSmallestAndLargest(int number1, int number2, int number3) {
+        int smallest = Math.min(number1, Math.min(number2, number3));
+        int largest = Math.max(number1, Math.max(number2, number3));
+        return new int[]{smallest, largest};
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        int num3 = input.nextInt();
+        
+        int[] results = findSmallestAndLargest(num1, num2, num3);
+        
+        System.out.println("Smallest: " + results[0]);
+        System.out.println("Largest: " + results[1]);
+        
+        input.close();
+    }
+}
